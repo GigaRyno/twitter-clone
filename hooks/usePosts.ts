cae240aba476b1b8d656d823d75001443cf6b8f3
Ryan LaVigne <ryan.lavigne@mymail.champlain.edu>
2024-03-29 16:27:08 -0400

    Cleaned up the code

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -1,6 +1,5 @@
 import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
-import { useSession } from 'next-auth/react';
 
 const usePosts = (userId?: string) => {
     const url =  userId ? `/api/posts?userId=${userId}` : '/api/posts';``
