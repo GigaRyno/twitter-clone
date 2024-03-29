9a8e4e8549b78e349f4bc93d57377e04e576cbf7
Ryan LaVigne <ryan.lavigne@mymail.champlain.edu>
2024-03-29 11:54:19 -0400

    Reworked PostFeed and PostContainer
    Reworked them to be more efficient and easier to read

--- twitter-clone/components/posts/PostContainer.tsx
+++ twitter-clone/components/posts/PostContainer.tsx
@@ -4,25 +4,15 @@ import PostItem from './PostItem';
 interface PostContainerProps {
   data: Record<string, any>;
   userId?: string;
-  retweetUser?: Record<string, any>;
 }
 
 const PostContainer: React.FC<PostContainerProps> = ({ data, userId }) => {
-
-    console.log('PostContainer Data: ', data);
-
-    if(data.post) {
-        return <RetweetItem userId={userId} data={data} post={data.post}/>
-    } else if(data) {
+    if(data && !data?.post) {
         return <PostItem userId={userId} data={data}/>
-    } 
-
-    // return (
-    //     <>
-    //         {isRetweet && <RetweetItem userId={userId} data={data} post={data}/>}
-    //         {isPost && <PostItem userId={userId} data={data}/>}
-    //     </>
-    // )
+    }
+    if(data?.post) {
+        return <RetweetItem userId={userId} data={data} post={data.post}/>
+    }
 }
 
 export default PostContainer;
--- twitter-clone/components/posts/PostFeed.tsx
+++ twitter-clone/components/posts/PostFeed.tsx
@@ -1,22 +1,37 @@
 import usePosts from '@/hooks/usePosts';
 import PostContainer from './PostContainer';
-import useRetweets from '@/hooks/useRetweetPosts';
+import useRetweetsPosts from '@/hooks/useRetweetPosts';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 interface PostFeedProps {
     userId?: string;
+    isProfile?: boolean;
 }
 
-const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
-    const { data: posts = [] } = usePosts(userId);
-    const { data: retweets = [] } = useRetweets(userId);
+const PostFeed: React.FC<PostFeedProps> = ({ userId, isProfile }) => {
+    const { data: currentUser } = useCurrentUser();
+    const { data: posts = [] } = usePosts(currentUser?.userId);
+    const { data: retweets = [] } = useRetweetsPosts(currentUser?.followingIds);
 
-    // sorts the posts and retweets by date
-    const sortedPosts = [...posts, ...retweets].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+    // Defined filtering for the posts and retweets that the user is following
+    const filteredposts = posts.filter((post: { userId: any; }) => currentUser?.followingIds.includes(post.userId));
+    const filteredRetweets = retweets.filter((retweet: { userId: any; }) => currentUser?.followingIds.includes(retweet.userId));
 
+    // Defined filtering for the posts and retweets that are from the user for the profile page
+    const profilePosts = posts.filter((post: { userId: any; }) => post.userId === userId);
+    const profileRetweets = retweets.filter((retweet: { userId: any; }) => retweet.userId === userId);
+
+    // Defined conditional for the posts and retweets
+    let isProfilePosts = isProfile ? profilePosts : filteredposts;
+    let isProfileRetweets = isProfile ? profileRetweets : filteredRetweets;
+
+    // sort the posts and retweets by the date
+    const sortedPosts = [...isProfilePosts, ...isProfileRetweets].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+    
     return (
         <>
             {sortedPosts.map((post: Record<string, any>) => (
-                <PostContainer userId={post.userId} key={post.id} data={post}/>
+                <PostContainer userId={post?.userId} key={post?.id} data={post}/>
             ))}
         </>
     )
